802bdd2d3f3e71f59b2058e129251f70858e128c
Ahmet Yasar <boss@Ahmet.local>
2014-05-23 18:43:36 -0400

    ex 10 and ex11 work

--- ex10.rb
+++ ex10.rb
@@ -2,11 +2,24 @@ def mutate_test(m)
   m.push(7)
 end
 
-
 number = [1,5]
 puts "These numbers are before mutate #{number}"
-
-
 mutate_test(number)
 puts "These numbers are after putate #{number}"
+
+
+# mutate array in method
+a= [1,2,3,4,5]
+
+def mutating(mutate)
+  mutate.delete(2)
+end
+puts
+puts 'second example'
+puts
+puts a #before mutate
+puts
+mutating(a)
+puts a
+
 
---
+++ ex11.rb
@@ -0,0 +1,14 @@
+
+def change_string(mutater)
+    mutater = 'This is exercise 11'
+end
+
+a = "Ready to move ex12?"
+change_string(a)
+puts a
+
+
+
+
+
+
